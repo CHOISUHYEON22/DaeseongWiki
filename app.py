c6ff5cecc03c126bff994ba88eac43588dba2434
Suhyeon CHOI <perduephantom@protonmail.ch>
2020-08-09 22:20:03 +0900

    to change code and design to simplify.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from search_process import simple_search_process, detail_search_process, resort_by_sorting_type_change, new_contents_process
+from search_process import sort_bunch, search_simple, search_detail, new_contents_process
 from flask import Flask, render_template, request, redirect, url_for
+from collections import namedtuple
 from datetime import datetime
 from time import strftime
 import pickle
@@ -7,127 +8,76 @@ import pickle
 app = Flask(__name__)
 
 
-#def list_category() -> tuple: return tuple({i[4] for i in get_data()})
+def list_category() -> list: return list({i[4] for i in gain_data()})
 
 
-#def list_title() -> tuple: return tuple({i[0] for i in get_data()})
+def list_title() -> list: return list({i[0] for i in gain_data()})
 
-def get_data() -> tuple:
+
+def find_index_by_title(data: list, cnt_title: str) -> int:
+
+    return next((i for i, v in enumerate(data) if v[0] == cnt_title))
+
+
+def gain_data() -> list:
 
     with open("./DATA.txt", "rb") as f: return pickle.load(f)
 
 
-def save_data(data: tuple) -> None:
-
-    with open("./DATA.txt", "wb") as f: pickle.dump(data, f)
-
-
 @app.route('/')
-def index(): return render_template('index.html', data=get_data())
+def index():
+
+    sorts: namedtuple = sort_bunch(new_contents_process(gain_data()))
+
+    return render_template('index_n.html', date_sort=sorts.date, r_date_sort=sorts.r_date, spell_sort=sorts.spell, title='최신 업로드', utility_sort=sorts.utility, list_category=list_category(), did_find=bool(sorts[0]))
 
 
 @app.route('/search', methods=['GET'])
 def search():
 
-    if request.args.get('simple_or_detail') == 'S':
+    if request.args.get('simple_or_detail') == 'S': sorts: namedtuple = sort_bunch(search_simple(gain_data(), request.args.get('search_term')))
 
-        search_term = request.args.get('search_term')
+    else: sorts: namedtuple = sort_bunch(search_detail(gain_data(), *(request.args.get(i) for i in ('start_when', 'end_when', 'category'))))
 
-        return render_template('search_simple.html', result=simple_search_process(search_term, get_data()), search_term=search_term, file_name='search_simple.html', data=get_data())
-
-    else:
-
-        cond = tuple(request.args.get(i) for i in ('start_when', 'end_when', 'category', 'utility')) + (get_data(),)
-
-        return render_template('search_detail.html', start_when=cond[0], end_when=cond[1], result=detail_search_process(*cond), file_name='search_detail.html')
-
-
-@app.route('/type', methods=['GET'])
-def type():
-
-    file_name = request.args.get('file_name')
-
-    result = resort_by_sorting_type_change(eval(request.args.get('result')), request.args.get('type'), get_data())
-
-    if file_name == 'search_simple.html': return render_template(file_name, result=result, search_term=request.args.get('search_term'), file_name=file_name, data=get_data())
-
-    else: return render_template(file_name, this_year=request.args.get('this_year'), result=result, file_name=file_name, data=get_data())
+    return render_template('index_n.html', date_sort=sorts.date, r_date_sort=sorts.r_date, spell_sort=sorts.spell, utility_sort=sorts.utility, list_category=list_category(), did_find=bool(sorts[0]))
 
 
 @app.route('/add')
-def add(): return render_template("add_content.html")
+def add(): return render_template('add_content_n.html', list_category=list_category())
+
+
+@app.route('/modify', methods=['GET'])
+def modify():
+
+    title, category, content = (request.args.get(i) for i in ('title', 'amp;category', 'amp;content'))
+
+    return render_template('modify_content_n.html', title=title, category=[category], content=content)
 
 
 @app.route('/process_add_modify', methods=['POST'])
-def process_add_modify():
+def process_add_modify() -> None:
 
-    content, data = request.form['content'].strip(), get_data()
+    data = gain_data()
 
-    if request.form['add_or_modify'] == 'A':
+    title, content, category = (request.form[i].strip() for i in ('title', 'content', 'category'))
 
-        title, category = request.form['title'].strip(), request.form['category']
+    if request.form['add_or_modify'] == 'M':
 
-        data += (title, strftime('%y-%m-%d %H:%M:%S'), content, 1, category, [content])
+        index_num = find_index_by_title(data, title)
 
-        redirect_add_modify(data)
+        data[index_num] = [title, strftime('%y-%m-%d %H:%M:%S'), content, data[index_num][3], category, data[index_num][5].append(content)]
 
-    else:
+    else: data.append([title, strftime('%y-%m-%d %H:%M:%S'), content, (0, 1), category, [content]])
 
-        index = int(request.form['index'][:-1])
-
-        data[index] = data[index][0] + (strftime('%y-%m-%d %H:%M:%S'), content) + data[index][3:5] + data[index][5].append(content)
-
-        redirect_add_modify(data, index)
+    with open("./DATA.txt", "wb") as f: pickle.dump(data, f)
 
 
-def redirect_add_modify(data: tuple, index: int = None):
+@app.route('/content')
+def display_content():
 
-    save_data(data)
+    data = gain_data()
 
-    return redirect(url_for('info', info_title=data[index][0] if index else url_for('new_contents')))
-
-
-@app.route('/modification', methods=['POST'])
-def modification():
-
-    data = get_data()
-
-    index = int(request.form['index'])
-
-    title, content, utility, category = (data[index][i] for i in (0, 2, 3, 4))
-
-    return render_template("Modification.html", list_category=list_category(), index=index, title=title, content=content, category=category, utility=utility)
-
-
-@app.route("/menu")
-def menu(): return render_template('search_detail.html', this_year=datetime.now().year)
-
-
-@app.route("/new_contents")
-def new_contents():
-
-    data = get_data()
-
-    return render_template('pre_in_menu.html', start_when=datetime.now().year, end_when=datetime.now().year,  data=new_contents_process(data, len(data)))
-
-
-@app.route("/info/<info_title>")
-def info(info_title):
-    return render_template('info.html', info=[get_data()[list_title().index(info_title)][i] for i in range(6)], index=get_data()[0].index(info_title), info_title=info_title)
-
-
-@app.route("/utility_calc", methods=['POST'])
-def utility_calc():
-
-    data = get_data()
-
-    info_title = request.form['info_title']
-
-    data[5][int(request.form['index'])] = (int(data[5][int(request.form['index'])]) + int(request.form['help'])) / 2
-
-    with open("DATA.txt", "wb") as f: pickle.dump(data, f)
-
-    return render_template('info.html', Info=[get_data()[i][get_data()[0].index(info_title)] for i in range(6)], index=get_data()[0].index(info_title), infoTitle=info_title)
+    return render_template('content_n.html', content=data[find_index_by_title(data, request.args.get('cnt_title'))])
 
 
 if __name__ == '__main__':
--- search_process.py
+++ search_process.py
@@ -1,52 +1,43 @@
 from clojure_fn import group_by
-
-# <대전제>
-## (((제목), (날짜), (내용), (별점), (분류), [히스토리]), ... )
-## 출력할 때를 제외하고 모든 데이터의 이동은 인덱스로 한다.
+from collections import namedtuple
 
 
 def sort_macro(index_of_standard: int, want2reverse: bool):
 
-    def sort_fn(data_i: tuple, data: tuple):
+    def sort_fn(data_searched_to_sort: list) -> list:
 
-        x_data: dict = group_by(lambda x: data[x][index_of_standard], data_i)
+        x_data: dict = group_by(lambda x: x[index_of_standard], data_searched_to_sort)
 
-        return tuple(i for k in sorted(x_data.keys(), reverse=want2reverse) for i in x_data[k])
+        return [i for k in sorted(x_data.keys(), reverse=want2reverse) for i in x_data[k]]
 
     return sort_fn
 
 
-def search_simple(search_term: str, data: tuple) -> tuple: return tuple(i for i, v in enumerate(data) if search_term in ''.join(v[:3]))
+def search_simple(data: list, search_term: str) -> list: return [v for i, v in enumerate(data) if search_term in ''.join(v[:3])]
 
 
-def search_detail(start_when: str, end_when: str, category: str, utility: str, data: tuple) -> tuple:
+def search_detail(data: list, start_when: str, end_when: str, category: str) -> list:
 
-    return tuple(i for i, v in enumerate(data) if all((v[1][:2] >= start_when[2:], v[1][:2] <= end_when[2:], v[3] >= int(utility), v[4] == category)))
-
-
-def spell_sort(data_i: tuple, data: tuple) -> tuple: return sort_macro(0, False)(data_i, data)
+    return [v for v in data if all((v[1][:8] >= start_when[2:], v[1][:8] <= end_when[2:], v[4] == category))]
 
 
-def r_date_sort(data_i: tuple, data: tuple) -> tuple: return sort_macro(1, False)(data_i, data)
+def spell_sort(data_searched_to_sort: list) -> list: return sort_macro(0, False)(data_searched_to_sort)
 
 
-def date_sort(data_i: tuple, data: tuple) -> tuple: return sort_macro(1, True)(data_i, data)
+def r_date_sort(data_searched_to_sort: list) -> list: return sort_macro(1, False)(data_searched_to_sort)
 
 
-def utility_sort(data_i: tuple, data: tuple) -> tuple: return sort_macro(3, True)(data_i, data)
+def date_sort(data_searched_to_sort: list) -> list: return sort_macro(1, True)(data_searched_to_sort)
 
 
-def simple_search_process(search_term: str, data: tuple) -> tuple: return date_sort(search_simple(search_term, data), data)
+def utility_sort(data_searched_to_sort: list) -> list: return sort_macro(3, True)(data_searched_to_sort)
 
 
-def detail_search_process(start_when: str, end_when: str, category: str, utility: str, data: tuple) -> tuple:
-
-    return date_sort(search_detail(start_when, end_when, category, utility, data), data)
+def new_contents_process(data: list) -> list: return data[-10 if len(data) > 10 else 0:]
 
 
-def new_contents_process(data: tuple, len_data: int) -> tuple: return date_sort(tuple(range((len_data - 10) if len_data > 10 else 0, len_data)), data)
+def sort_bunch(data_searched_to_sort: list) -> namedtuple:
 
+    fn_names = namedtuple('fn_names', 'date r_date spell utility')
 
-def resort_by_sorting_type_change(result: tuple, type: str, data: tuple) -> tuple:
-
-    return next((i for i in (date_sort, r_date_sort, utility_sort, spell_sort) if i.__name__[:-5] == type))(result, data)
+    return fn_names(**{f.__name__[:-5]: f(data_searched_to_sort) for f in (date_sort, r_date_sort, spell_sort, utility_sort)})
